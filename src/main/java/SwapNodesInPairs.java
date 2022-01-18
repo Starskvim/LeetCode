5c398a28266b8d54840aad5ca232a136e203fa64
Mikhail Gaivola <starskvim@mail.ru>
2022-01-18 22:37:58 +0300

    Added problem

---
+++ src/main/java/SwapNodesInPairs.java
@@ -0,0 +1,43 @@
+public class SwapNodesInPairs {
+
+    public static void main(String[] args) {
+
+    }
+
+    public static ListNode swapPairs(ListNode head) {
+        if (head == null || head.next == null) return head;
+        ListNode cur = head;
+        ListNode newHead = head.next;
+        while (cur != null && cur.next != null) {
+            ListNode mem = cur;
+            cur = cur.next;
+
+            mem.next = cur.next;
+            cur.next = mem;
+
+            cur = mem.next;
+            if (cur != null && cur.next != null) {
+                mem.next = cur.next;
+            }
+        }
+        return newHead;
+    }
+
+    public class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode() {
+        }
+
+        ListNode(int val) {
+            this.val = val;
+        }
+
+        ListNode(int val, ListNode next) {
+            this.val = val;
+            this.next = next;
+        }
+    }
+
+}
